c1e1db2587d4e81727dce34f4763bc3b71181cec
Peter Meehan <meehanp2@tcd.ie>
2018-02-24 14:30:32 +0000

    mix new --sup

---
+++ lib/relay/application.ex
@@ -0,0 +1,20 @@
+defmodule Relay.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Relay.Worker.start_link(arg)
+      # {Relay.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Relay.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
